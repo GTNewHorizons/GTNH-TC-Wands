594a59a2b1ce318d68d50a72b640dca9390bc156
ScriptedPiky <41557212+ScriptedPiky@users.noreply.github.com>
2021-09-16 14:23:40 -0700

    Update GTWandRegistry.java
    Re-arranges the tiering of most wands

--- src/main/java/com/gtnewhorizons/tcwands/GTWandRegistry.java
+++ src/main/java/com/gtnewhorizons/tcwands/GTWandRegistry.java
@@ -17,15 +17,15 @@ public class GTWandRegistry implements IWandRegistry {
     @Override
     public void register() {
         new WandRecipeCreator("wood").regWandRecipe(0, 5, GT_ModHandler.getModItem("Forestry", "oakStick", 1, 0, new ItemStack(Items.stick)), GTTier.MV).regSceptreRecipe(2.0F);
-        new WandRecipeCreator("greatwood").regWandRecipe(20, 5, HV).regSceptreRecipe(2F).regUpwardStaffRecipe(75, 15).regStaffSceptreRecipe(1.4F);
-        new WandRecipeCreator("reed").regWandRecipe(50, 10, EV).regSceptreRecipe(1.5F).regUpwardStaffRecipe(125, 15).regStaffSceptreRecipe(1.5F);
-        new WandRecipeCreator("blaze").regWandRecipe(50, 10, EV).regSceptreRecipe(1.5F).regUpwardStaffRecipe(125, 15).regStaffSceptreRecipe(1.5F);
-        new WandRecipeCreator("obsidian").regWandRecipe(50, 10, EV).regSceptreRecipe(1.5F).regUpwardStaffRecipe(125, 15).regStaffSceptreRecipe(1.5F);
-        new WandRecipeCreator("ice").regWandRecipe(50, 10, EV).regSceptreRecipe(1.5F).regUpwardStaffRecipe(125, 15).regStaffSceptreRecipe(1.5F);
-        new WandRecipeCreator("quartz").regWandRecipe(50, 10, EV).regSceptreRecipe(1.5F).regUpwardStaffRecipe(125, 15).regStaffSceptreRecipe(1.5F);
-        new WandRecipeCreator("bone").regStaffRecipe(125, 15, IV).regStaffSceptreRecipe(1.5F);//FIXME Why only staff?
-        new WandRecipeCreator("silverwood").regWandRecipe(75, 15, IV).regSceptreRecipe(1.4F).regUpwardStaffRecipe(150, 15).regStaffSceptreRecipe(1.2F);
-        new WandRecipeCreator("primal").regStaffRecipe(175, 20, ZPM).regStaffSceptreRecipe(1.6F);
+        new WandRecipeCreator("greatwood").regWandRecipe(20, 5, MV).regSceptreRecipe(2F).regUpwardStaffRecipe(75, 15).regStaffSceptreRecipe(1.4F);
+        new WandRecipeCreator("reed").regWandRecipe(50, 10, HV).regSceptreRecipe(1.5F).regUpwardStaffRecipe(125, 15).regStaffSceptreRecipe(1.5F);
+        new WandRecipeCreator("blaze").regWandRecipe(50, 10, HV).regSceptreRecipe(1.5F).regUpwardStaffRecipe(125, 15).regStaffSceptreRecipe(1.5F);
+        new WandRecipeCreator("obsidian").regWandRecipe(50, 10, HV).regSceptreRecipe(1.5F).regUpwardStaffRecipe(125, 15).regStaffSceptreRecipe(1.5F);
+        new WandRecipeCreator("ice").regWandRecipe(50, 10, HV).regSceptreRecipe(1.5F).regUpwardStaffRecipe(125, 15).regStaffSceptreRecipe(1.5F);
+        new WandRecipeCreator("quartz").regWandRecipe(50, 10, HV).regSceptreRecipe(1.5F).regUpwardStaffRecipe(125, 15).regStaffSceptreRecipe(1.5F);
+        new WandRecipeCreator("bone").regWandRecipe(50, 10, HV).regSceptreRecipe(1.5F).regUpwardStaffRecipe(125, 15).regStaffSceptreRecipe(1.5F);
+        new WandRecipeCreator("silverwood").regWandRecipe(75, 15, EV).regSceptreRecipe(1.65).regUpwardStaffRecipe(150, 15).regStaffSceptreRecipe(1.75F);
+        new WandRecipeCreator("primal").regStaffRecipe(175, 20, IV).regStaffSceptreRecipe(1.6F);
 
         TCWandAPI.regCap(new CapWrapper("iron", 0));
         TCWandAPI.regCap(new CapWrapper("copper", 1));
@@ -38,11 +38,11 @@ public class GTWandRegistry implements IWandRegistry {
             TCWandsMod.LOGGER.info("Detected Forbidden Magic. Applying GTNH Recipes...");
             new WandRecipeCreator("profane").regWandRecipe(25, 5, HV).regSceptreRecipe(2F);
             new WandRecipeCreator("tainted").regWandRecipe(125, 15, IV).regSceptreRecipe(1.5F);
-            new WandRecipeCreator("blood").regWandRecipe(125, 15, IV).regSceptreRecipe(1.5F).regUpwardStaffRecipe(150, 15).regStaffSceptreRecipe(1.2F);
+            new WandRecipeCreator("blood").regWandRecipe(125, 15, EV).regSceptreRecipe(1.5F).regUpwardStaffRecipe(150, 15).regStaffSceptreRecipe(1.2F);
             new WandRecipeCreator("infernal").regWandRecipe(125, 15, IV).regSceptreRecipe(1.5F);
-            new WandRecipeCreator("livingwood").regWandRecipe(75, 15, IV).regSceptreRecipe(1.4F);
-            new WandRecipeCreator("dreamwood").regWandRecipe(75, 15, IV).regSceptreRecipe(1.4F).regUpwardStaffRecipe(150, 15).regStaffSceptreRecipe(1.2F);
-            new WandRecipeCreator("witchwood").regWandRecipe(75, 15, IV).regSceptreRecipe(1.4F).regUpwardStaffRecipe(150, 15).regStaffSceptreRecipe(1.2F);
+            new WandRecipeCreator("livingwood").regWandRecipe(75, 15, EV).regSceptreRecipe(1.4F);
+            new WandRecipeCreator("dreamwood").regWandRecipe(75, 15, EV).regSceptreRecipe(1.4F).regUpwardStaffRecipe(150, 15).regStaffSceptreRecipe(1.2F);
+            new WandRecipeCreator("witchwood").regWandRecipe(75, 15, EV).regSceptreRecipe(1.4F).regUpwardStaffRecipe(150, 15).regStaffSceptreRecipe(1.2F);
 
             TCWandAPI.regCap(new CapWrapper("manasteel", 5));
             TCWandAPI.regCap(new CapWrapper("terrasteel", 1));
@@ -60,7 +60,7 @@ public class GTWandRegistry implements IWandRegistry {
 
         if (CompatibleMods.BLOOD_ARSENAL.isPresent()) {
             TCWandsMod.LOGGER.info("Detected Blood Arsenal. Applying GTNH Recipes...");
-            new WandRecipeCreator("blood_wood").regWandRecipe(130, 15, IV).regSceptreRecipe(1.2F).regUpwardStaffRecipe(175, 20).regStaffSceptreRecipe(1.6F);
+            new WandRecipeCreator("blood_wood").regWandRecipe(130, 15, EV).regSceptreRecipe(1.2F).regUpwardStaffRecipe(175, 20).regStaffSceptreRecipe(1.6F);
 
             TCWandAPI.regCap(new CapWrapper("blood_iron", 6));
         }
@@ -71,7 +71,7 @@ public class GTWandRegistry implements IWandRegistry {
             TCWandAPI.makeCap("shadowcloth", GT_ModHandler.getModItem("TaintedMagic", "ItemWandCap", 1, 3), 0.85F, 7, new ResourceLocation("taintedmagic", "textures/models/ModelWAND_CAP_SHADOW_CLOTH.png"));
             TCWandAPI.makeCap("crimsoncloth", GT_ModHandler.getModItem("TaintedMagic", "ItemWandCap", 1, 2), 0.80F, 9, new ResourceLocation("taintedmagic", "textures/models/ModelWAND_CAP_CRIMSON_CLOTH.png"));
 
-            new WandRecipeCreator("warpwood").regWandRecipe(135, 15, IV).regSceptreRecipe(1.2F).regUpwardStaffRecipe(200, 25).regStaffSceptreRecipe(1.2F);
+            new WandRecipeCreator("warpwood").regWandRecipe(135, 15, LUV).regSceptreRecipe(1.2F).regUpwardStaffRecipe(200, 25).regStaffSceptreRecipe(1.2F);
 
             TCWandAPI.regCap(new CapWrapper("cloth", 3));
             TCWandAPI.regCap(new CapWrapper("crimsoncloth", 6));
@@ -81,8 +81,8 @@ public class GTWandRegistry implements IWandRegistry {
 
         if (CompatibleMods.THAUMIC_BASES.isPresent()) {
             TCWandsMod.LOGGER.info("Detected Thaumic Bases. Applying GTNH Recipes...");
-            new WandRecipeCreator("tbthaumium").regWandRecipe(60, 10, EV).regSceptreRecipe(1.5F);
-            new WandRecipeCreator("tbvoid").regWandRecipe(160, 15, IV).regSceptreRecipe(1.2F);
+            new WandRecipeCreator("tbthaumium").regWandRecipe(60, 10, MV).regSceptreRecipe(1.5F);
+            new WandRecipeCreator("tbvoid").regWandRecipe(160, 15, HV).regSceptreRecipe(1.2F);
 
             TCWandAPI.regCap(new CapWrapper("thauminite", 6));
         }
@@ -90,8 +90,8 @@ public class GTWandRegistry implements IWandRegistry {
 
         if (CompatibleMods.THAUMIC_EXPLORATION.isPresent()) {
             TCWandsMod.LOGGER.info("Detected Thaumic Exploration. Applying GTNH Recipes...");
-            new WandRecipeCreator("AMBER").regWandRecipe(20, 5, HV).regSceptreRecipe(2F).regUpwardStaffRecipe(75, 15).regStaffSceptreRecipe(1.4F);
-            new WandRecipeCreator("TRANSMUTATION").regWandRecipe(50, 10, EV).regSceptreRecipe(1.5F).regUpwardStaffRecipe(125, 15).regStaffSceptreRecipe(1.5F);
+            new WandRecipeCreator("AMBER").regWandRecipe(20, 5, MV).regSceptreRecipe(2F).regUpwardStaffRecipe(75, 15).regStaffSceptreRecipe(1.4F);
+            new WandRecipeCreator("TRANSMUTATION").regWandRecipe(50, 10, HV).regSceptreRecipe(1.5F).regUpwardStaffRecipe(125, 15).regStaffSceptreRecipe(1.5F);
             //FIXME where's NECROMANCER
             //cores.add(new WandCore("BREAD",HV, LICH, 20, 5, 2F));//FIXME need or not need?
 
